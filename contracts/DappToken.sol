4e25da6cf60c6ae8378312854b5bf1361531a260
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2021-11-28 17:56:37 +0100

    setting up the truffle project
    setting up the truffle project and sol files

---
+++ contracts/DappToken.sol
@@ -0,0 +1,52 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+
+contract SafeMath {
+    function safeAdd(uint a, uint b) public pure returns (uint c) {
+        c = a + b;
+        require(c >= a);
+    }
+
+    function safeSub(uint a, uint b) public pure returns (uint c) {
+        require(b <= a);
+        c = a - b;
+    }
+
+    function safeMul(uint a, uint b) public pure returns (uint c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+
+    function safeDiv(uint a, uint b) public pure returns (uint c) {
+        require(b > 0);
+        c = a / b;
+    }
+}
+
+/**
+ERC Token Standard #20 Interface
+https://github.com/ethereum/EIPs/blob/master/EIPS/eip-20-token-standard.md
+*/
+contract ERC20Interface {
+    function totalSupply() public constant returns (uint);
+    function balanceOf(address tokenOwner) public constant returns (uint balance);
+    function allowance(address tokenOwner, address spender) public constant returns (uint remaining);
+    function transfer(address to, uint tokens) public returns (bool success);
+    function approve(address spender, uint tokens) public returns (bool success);
+    function transferFrom(address from, address to, uint tokens) public returns (bool success);
+
+    event Transfer(address indexed from, address indexed to, uint tokens);
+    event Approval(address indexed tokenOwner, address indexed spender, uint tokens);
+}
+
+contract DappToken {
+    // Contructor
+    // Set the total number of tokens
+    // read the total number of tokens
+    uint256 public totalSupply;
+
+    constructor() public {
+        totalSupply = 1000000;
+    }
+}
