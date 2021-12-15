ab88bd1da23fc8625a5942c172229932fc5d466e
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2021-12-15 09:35:21 +0100

    Update layout.jsx

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -7,16 +7,17 @@ import '../style/bootstrap.min.css';
 import '../style/fontawesome-free-5.15.4-web/css/all.min.css';
 import '../style/style.css';
 
-const Layout = ({ children, loginFn, isLoggedIn, accounts, title, author }) => (
-    <Container className={'main_wrapper'} fluid>
-        <div className={'content_wrapper'}>
-            <Header title={title} loginFn={loginFn} isLoggedIn={isLoggedIn} accounts={accounts} />
-            {children}
-        </div>
-        <Footer author={author} />
-    </Container>
-);
-
+const Layout = ({ children, loginFn, isLoggedIn, accounts, title, author, network }) => {
+    return (
+        <Container className={'main_wrapper'} fluid>
+            <div className={'content_wrapper'}>
+                <Header loginFn={loginFn} isLoggedIn={isLoggedIn} accounts={accounts} title={title} network={network} />
+                {children}
+            </div>
+            <Footer author={author} />
+        </Container>
+    );
+}
 
 export default Layout;
 
