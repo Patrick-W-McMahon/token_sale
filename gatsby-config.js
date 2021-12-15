e90a370d7fa16ccafbdd63babd9ebca9c5391633
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2021-12-15 09:34:26 +0100

    setting up network data

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,7 +12,16 @@ module.exports = {
         tokenPrice: 0.05, //price in Eth
         tokenSupply: 750000,
         icoStart: 10000, //start time in blocks 
-        icoEnd: false //end time in blocks, false if end when supply is all sold.
+        icoEnd: false, //end time in blocks, false if end when supply is all sold.
+        networks: {
+            mainNet: {
+                tokenAddress: '0xd00981105e61274c8a5cd5a88fe7e037d935b513',
+                tokenSymbol: 'DAPP',
+                tokenDecimals: 18,
+                tokenImage: 'http://placekitten.com/200/300'
+            }
+        }
+
     },
     plugins: [],
 }
