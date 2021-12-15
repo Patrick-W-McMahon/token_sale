fe8d54abccd943a2a206309d201ee662e9cfb3d8
Patrick W. McMahon <patrick.w.mcmahon@gmail.com>
2021-12-15 09:35:07 +0100

    footer updates

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Container } from "react-bootstrap";
 
-const Footer = ({ author }) => (
-    <Container>
-        <div>
-        <p>Created by {author} &#169; 2021</p>
-        </div>
-    </Container>
-);
+const Footer = ({ author }) => {
+    return (
+        <Container className={'footer-container'} fluid>
+            <Container>
+                <p>Created by {author} &#169; 2021</p>
+            </Container>
+        </Container>
+    );
+}
 
 export default Footer;
